1ed0b7df1393cbe301f7075edc22675be8bbcfd3
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-06-11 13:25:13 +0530

    [fix] hof mobile view

--- app/components/dashboard-components/hallOfFame.tsx
+++ app/components/dashboard-components/hallOfFame.tsx
@@ -57,7 +57,7 @@ const HallOfFame = () => {
         <div className="text-center text-red-500">{error}</div>
       ) : (
         <div className="flex-1 overflow-y-auto pr-1 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-transparent">
-          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-4">
             {languages.map((lang) => (
               <LanguagePill
                 key={lang}
